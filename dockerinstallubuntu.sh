71869976c2be6e2c2991192f4c1061df589f70a0
ryanblack <ryanblack@inbox.ru>
2019-07-12 16:51:15 +0000

    docker ubuntu install added

---
+++ dockerinstallubuntu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt-get update
+apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install docker-ce
+adduser user
+usermod -aG docker user
+systemctl enable docker

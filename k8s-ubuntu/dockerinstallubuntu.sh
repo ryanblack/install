b5554909135f904b6875a11ca5e8ffccbbb5e785
root <root@ryanblack3c.mylabserver.com>
2019-08-21 02:12:24 +0000

    some updates

--- k8s-ubuntu/dockerinstallubuntu.sh
+++ k8s-ubuntu/dockerinstallubuntu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-apt-get update
-apt-get install apt-transport-https ca-certificates curl software-properties-common
+apt-get -y update
+apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install docker-ce
+apt-get -y update
+apt-get -y install docker-ce
 adduser user
 usermod -aG docker user
 systemctl enable docker
